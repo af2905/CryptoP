765ecc307cf31c62d552dc60da02f063a657c306
aifrolov <aifrolov@yoomoney.ru>
2022-12-07 21:04:06 +0100

    Initial commit

---
+++ app/src/main/java/com/github/af2905/cryptop/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.af2905.cryptop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
